fbaf941521c4e96d47e9ce375718d6fbbd181443
Madhav Kumar <madhavampire@gmail.com>
2023-07-15 12:16:45 +0530

    init2

---
+++ app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask, render_template, request, redirect, url_for, session
+import hashlib
+import os 
+
+
+app = Flask(__name__)
+app.secret_key = os.environ["FLASK_SECRET_KEY"]  # Change this to a strong random key in a production environment
+
+# Sample admin credentials (replace with your actual admin credentials)
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = hashlib.sha256('password'.encode()).hexdigest()  # Hashed password for "password"
+
+def is_authenticated(username, password):
+    # Compare the provided username and hashed password with the actual admin credentials
+    return username == ADMIN_USERNAME and hashlib.sha256(password.encode()).hexdigest() == ADMIN_PASSWORD
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if is_authenticated(username, password):
+            # Save the authenticated status in the session
+            session['authenticated'] = True
+            return redirect(url_for('dashboard'))
+        else:
+            error = 'Invalid credentials. Please try again.'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    # Check if the user is authenticated in the session
+    if session.get('authenticated'):
+        return render_template('dashboard.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    # Clear the authenticated status from the session
+    session.pop('authenticated', None)
+    return redirect(url_for('login'))
+
+
+def start_server():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    start_server()
